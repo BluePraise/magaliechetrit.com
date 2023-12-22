b1fe1bc3241db67350fe1c39233f2698f044d298
Magalie Chetrit <email@magaliechetrit.com>
2023-12-22 16:04:35 -0500

    WIP - Blogposts

--- src/_data/blogPosts.js
+++ src/_data/blogPosts.js
@@ -1,14 +1,15 @@
-const EleventyFetch = require("@11ty/eleventy-fetch");
+const EleventyFetch = require("@11ty/eleventy-fetch")
 
 async function getBlogPosts() {
 
-    const url = `https://libra.nightschool.studio/mc/wp-json/wp/v2/posts`;
+    const url = `https://libra.nightschool.studio/mc/wp-json/wp/v2/posts`
     const response = EleventyFetch(url, {
         duration: "1d",
         type: "json"
     });
-    const articles = await response;
-    return articles;
+    const articles = await response
+    // console.log(articles)
+    return articles
 }
 
-module.exports = getBlogPosts;
+module.exports = getBlogPosts
