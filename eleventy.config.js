08fe55c2e4e5f16204fc72f1fc4460dfbdf8f776
Magalie Chetrit <email@magaliechetrit.com>
2024-01-24 13:14:29 -0500

    [DEV] Updated eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,24 @@
-const pluginRev = require("eleventy-plugin-rev")
-const { DateTime } = require("luxon")
+const pluginRev = require("eleventy-plugin-rev");
+const { DateTime } = require("luxon");
+
 
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(pluginRev)
-    eleventyConfig.setServerOptions({ showVersion: true })
+    eleventyConfig.addPlugin(pluginRev);
+
+    eleventyConfig.setServerOptions({ showVersion: true });
     // move css and jpg to output folder
-    eleventyConfig.addPassthroughCopy("src/assets/")
-    eleventyConfig.addWatchTarget("src/assets/css/*.css")
+    eleventyConfig.addPassthroughCopy("src/assets/blog/*/*.jpg");
+    eleventyConfig.addPassthroughCopy("src/assets/*/*");
+    eleventyConfig.addWatchTarget("src/assets/css/*.css");
 
     // presents current year
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`)
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     // presents modified date in format 01-01-2022
     eleventyConfig.addFilter("makeDateReadable", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-    })
+    });
 
 
     return {
